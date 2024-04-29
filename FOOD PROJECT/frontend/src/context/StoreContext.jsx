2cb47898c7db0b7b10f18584432dfeb6fb4ab6e1
AryanPatel126 <158442835+AryanPatel126@users.noreply.github.com>
2024-04-29 19:12:59 +0530

    added cart page and its functionality

--- FOOD PROJECT/frontend/src/context/StoreContext.jsx
+++ FOOD PROJECT/frontend/src/context/StoreContext.jsx
@@ -20,6 +20,18 @@ const StoreProvider = (props) => {
         setCartItems((prev) =>({...prev,[itemId]:prev[itemId]-1}))
       }
 
+      const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in cartItems){
+          if(cartItems[item]>0) {
+            let itemInfo = food_list.find((product)=>product._id === item)
+            totalAmount += itemInfo.price * cartItems[item];
+          }
+        }
+        return totalAmount;
+      }
+
+      // to check cart items
       useEffect(() =>{
         console.log(cartItems)
       },[cartItems])
@@ -31,6 +43,7 @@ const StoreProvider = (props) => {
       setCartItems,
       addToCart,
       removeFromCart,
+      getTotalCartAmount
   };
 
     
